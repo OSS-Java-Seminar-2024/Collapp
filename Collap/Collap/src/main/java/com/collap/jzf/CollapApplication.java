85f21b93cecbe95a5c635748ec63f7807928a5d8
JozoPejic <147092358+JozoPejic@users.noreply.github.com>
2024-11-10 20:46:41 +0100

    Add files via upload

---
+++ Collap/Collap/src/main/java/com/collap/jzf/CollapApplication.java
@@ -0,0 +1,13 @@
+package com.collap.jzf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollapApplication.class, args);
+    }
+
+}
---
+++ Collap/Collap/src/test/java/com/collap/jzf/CollapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collap.jzf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
